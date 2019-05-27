fae48740e2d167648a3b3dfaca0aedb74dd41197
Piotr Nowak <piotrn@highsolutions.pl>
2019-05-27 11:39:15 +0200

    [fix] add handle cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use App\Http\Middleware\ApiToken;
 use App\Http\Middleware\AuthenticateDataporten;
+use Barryvdh\Cors\HandleCors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -42,6 +43,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            HandleCors::class,
         ],
     ];
 
