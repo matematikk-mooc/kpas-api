3726dc92d40c09678e77ef9c7059774ef1fe9f22
marast78 <marek@highsolutions.pl>
2019-05-27 09:21:30 +0200

    [fix] worker view

--- resources/views/common/groupmemberships.blade.php
+++ resources/views/common/groupmemberships.blade.php
@@ -1,17 +1,18 @@
-<table class="table">
+<table class="table table-dark table-striped table-bordered">
     <tr>
         <th>Gruppekategori i Canvas</th>
         <th>Gruppemuligheter i dataporten</th>
     </tr>
-    @foreach(($groupCategories as $groupCategory)
+    @foreach($groupCategories as $groupCategory)
         <tr>
             <td>{{ $groupCategory->name }} </td>
+            <td>
                 <ul>
                 @foreach($groups as $dataportenGroup)
                     <li>{{ $dataportenGroup->displayName . "(" . $dataportenGroup->membership->basic . ")" }}</li>
                 @endforeach
                 </ul>
-            <td>{{ $dp }}</td>
+            </td>
         </tr>
     @endforeach
 </table>
