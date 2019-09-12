c089ac0869c3efcd15f7d16d656be7a4af951719
Piotr Nowak <piotrn@highsolutions.pl>
2019-09-12 14:33:19 +0200

    [fix] force ssl and remove redundant redirect

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] === '80') {
+            $_SERVER['HTTPS'] = 'ok';
+            $_SERVER['SERVER_PORT'] = 443;
+        }
     }
 
     /**
