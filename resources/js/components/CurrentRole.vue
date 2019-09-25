502331054bd38736a0192db0da377b9c949f8797
etsikt <erlend.thune@gmail.com>
2019-09-26 01:39:37 +0200

    Add CurrentRole.vue

---
+++ resources/js/components/CurrentRole.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-if="isPrincipal">
+        Du er registrert som skoleeier-/leder.
+    </div>
+    <div v-else>
+        Du er registrert som deltager.
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "CurrentRole",
+
+    props: {
+        isPrincipal: Boolean,
+    },
+  }
+</script>
