d913876946ccc57f3442b4bb686cdee305dd66a5
etsikt <erlend.thune@gmail.com>
2019-12-17 16:27:52 +0100

    Put statistics css in webpack.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/statistics.scss', 'public/css');
 
 mix.js('resources/js/app.js', 'public/js');
 
