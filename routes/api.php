ebcae077ad96a61d48e33d1a31388f699763a038
meklitia <me@aiascience.com>
2020-06-15 16:15:49 +0200

    clean api.php

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,10 @@
 Route::group(['prefix' => 'nsr'], function () {
     # Route::get('/counties', 'SchoolsController@counties');
     Route::get('/counties', 'SkolerController@all_fylke');
-
     # Route::get('/communities/{countyId}', 'SchoolsController@communities');
     Route::get('/communities/{fylkesnr}', 'SkolerController@kommuner');
-
     #Route::get('/schools/{communityId}', 'SchoolsController@schools');
     Route::get('/schools/{kommunenr}', 'SkolerController@skoler');
-
 });
 
 Route::get('counties', 'SkolerController@all_fylke');
@@ -48,13 +45,6 @@ Route::group(['prefix' => 'statistics'], function () {
     Route::get('/groupCategory/{categoryId}', 'StatisticsController@groupCategory');
     Route::get('/groupCategory/{categoryId}/count', 'StatisticsController@groupCategoryCount');
 });
-
-//
-//Route::group(['prefix' => 'nsr'], function () {
-//    Route::get('/counties', 'SchoolsController@counties');
-//    Route::get('/communities/{countyId}', 'SchoolsController@communities');
-//    Route::get('/schools/{communityId}', 'SchoolsController@schools');
-//});
 
 Route::group(['prefix' => 'enrollment', 'middleware' => 'lti'], function () {
     Route::get('/', 'EnrollmentController@index');
