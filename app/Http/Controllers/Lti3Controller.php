58775c5441efbadf7ad703778dca23a45f2b2eac
meklitia <me@aiascience.com>
2020-06-15 11:14:22 +0200

    user CanvasService method

--- app/Http/Controllers/Lti3Controller.php
+++ app/Http/Controllers/Lti3Controller.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\CanvasException;
 use App\Exceptions\LtiException;
 use App\Ltiv3\LTI3_Database;
-use GuzzleHttp\Exception\GuzzleException;
+use App\Services\CanvasService;
+use GuzzleHttp\Client;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\View\View;
 use IMSGlobal\LTI;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request as psr_request;
 
 /**
  * @lti3 LTI v 1.3 request management
@@ -60,11 +60,10 @@ class Lti3Controller extends Controller
         $settings["canvas_user_id"] = (string)$settings['canvas_user_id'];
         $settings["canvas_course_id"] = (string)$settings['canvas_course_id'];
         $settings["canvas_user_login_id"] = $launch->get_launch_data()['email'];
-        try{
+        try {
             $settings = $this->get_categories($settings);
 
-            } catch (GuzzleException $e)
-        {
+        } catch (CanvasException $e) {
             throw new LtiException("Error at LTIv3 get categories from canvas :" . $e->getMessage());
 
         }
@@ -84,41 +83,29 @@ class Lti3Controller extends Controller
      *  Get categories for a given course from canvas api
      * @param $settings
      * @return array
-     * @throws GuzzleException
+     * @throws CanvasException
      */
     private function get_categories($settings)
     {
-        $client = new Client();
-        $canvas = env('CANVAS_DOMAIN');
-        $canvas_access_key = env('CANVAS_ACCESS_KEY');
         $course = $settings["canvas_course_id"];
-        $headers = ['Authorization' => 'Bearer '.$canvas_access_key,];
-        $request = new psr_request('GET', $canvas . '/courses/' . $course . '/group_categories', $headers);
-        $response = $client->send($request)->getBody();
-        $items = json_decode((string) $response, true);
-
-        foreach ($items as  $value) {
-
-            if ($value['name'] == "Fylke") {
-                $settings["county_category_id"] = (string)$value['id'];
-            }
-            if ($value['name'] == "Skole") {
-                $settings["school_category_id"] = (string)$value['id'];
-            }
-            if ($value['name'] == "Kommune") {
-                $settings["community_category_id"] = (string)$value['id'];
-            }
-            if ($value['name'] == "Faggruppe i kommunen") {
-                $settings["county_faculty_category_id"] = (string)$value['id'];
-            }
-            if ($value['name'] == "Faggruppe i fylket") {
-                $settings["community_faculty_category_id"] = (string)$value['id'];
-            }
-            if ($value['name'] == "Leder/eier (fylke)") {
-                $settings["county_principals_category_id"] = (string)$value['id'];
-            }
-            if ($value['name'] == "Leder/eier (kommune") {
-                $settings["community_principals_category_id"] = (string)$value['id'];
+        $client = new Client();
+        $canvas_service = new CanvasService($client);
+        $categories = collect($canvas_service->getGroupCategories($course));
+        foreach ($categories as $value) {
+            if ($value->name == "Fylke") {
+                $settings["county_category_id"] = $value->id;
+            } elseif ($value->name == "Skole") {
+                $settings["school_category_id"] = $value->id;
+            } elseif ($value->name == "Kommune") {
+                $settings["community_category_id"] = (string)$value->id;
+            } elseif ($value->name == "Faggruppe i kommunen") {
+                $settings["county_faculty_category_id"] = (string)$value->id;
+            } elseif ($value->name == "Faggruppe i fylket") {
+                $settings["community_faculty_category_id"] = (string)$value->id;
+            } elseif ($value->name == "Leder/eier (fylke)") {
+                $settings["county_principals_category_id"] = (string)$value->id;
+            } elseif ($value->name == "Leder/eier (kommune") {
+                $settings["community_principals_category_id"] = (string)$value->id;
             }
         }
         return $settings;
