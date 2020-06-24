2fb00e9e26c679afc0f6e0d467a8f4627e6fe66e
meklitia <me@aiascience.com>
2020-06-24 15:14:18 +0200

    npm update

--- deploy.sh
+++ deploy.sh
@@ -145,6 +145,7 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   #npm install -g npm@6.14.5
   cd "$DEPLOYMENT_TARGET"
   npm -v
+  npm update
   npm install --production
   npm run production
   exitWithMessageOnError "Node install failed"
