e296f9f52f2116704a762aa48f209ec9b9b00c26
meklitia <me@aiascience.com>
2020-06-24 12:10:31 +0200

    what version of npm is in production

--- deploy.sh
+++ deploy.sh
@@ -144,6 +144,7 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   #apt-get install nodejs
   #npm install -g npm@6.14.5
   cd "$DEPLOYMENT_TARGET"
+  npm -v
   npm install --production
   npm run production
   exitWithMessageOnError "Node install failed"
