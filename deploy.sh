771b42db7cc407937797a35bca355cfd2e845293
mekupilp <meklit.dekita@bouvet.no>
2020-09-02 12:58:06 +0200

    nom only install

--- deploy.sh
+++ deploy.sh
@@ -145,10 +145,10 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   #npm install -g npm@6.14.5
   cd "$DEPLOYMENT_TARGET"
   npm -v
-  rm -Rf node_modules
   npm update
   npm install css-unit-converter
   npm install --production
+  npm install --global cross-env
   npm run production
   exitWithMessageOnError "Node install failed"
   # shellcheck disable=SC2164
