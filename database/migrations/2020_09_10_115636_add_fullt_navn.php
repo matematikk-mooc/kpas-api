610ee127655e55fdef2b0e4d3ce4ab970c1335ec
etsikt <erlend.thune@gmail.com>
2020-09-15 23:24:11 +0200

    Touch migration.

--- database/migrations/2020_09_10_115636_add_fullt_navn.php
+++ database/migrations/2020_09_10_115636_add_fullt_navn.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFulltNavn extends Migration
+class AddFulltNavn extends Migration 
 {
     /**
      * Run the migrations.
