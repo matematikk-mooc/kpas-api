2c6ca262d6598c10e8f93a631df82a2d9f81e9bc
styxianriverman <71879473+styxianriverman@users.noreply.github.com>
2020-10-20 17:01:05 +0200

    Add geocoordinates to schools and kindergartens

--- app/Barnehage.php
+++ app/Barnehage.php
@@ -9,7 +9,20 @@ class Barnehage extends Model
     //
     public $incrementing = false;
     protected $primaryKey = 'OrgNr';
-    protected $fillable = ['KommuneNr', 'Navn', 'FulltNavn', 'OrgNr', 'NSRId', 'FylkeNr', 'ErBarnehage', 'ErBarnehageEier', 'ErOffentligBarnehage','ErPrivatBarnehage'];
+    protected $fillable = [
+        'KommuneNr',
+        'Navn',
+        'FulltNavn',
+        'OrgNr',
+        'NSRId',
+        'FylkeNr',
+        'ErBarnehage',
+        'ErBarnehageEier',
+        'ErOffentligBarnehage',
+        'ErPrivatBarnehage',
+        'Breddegrad',
+        'Lengdegrad',
+    ];
 
     public function UpdateBarnehage($kindergarten) {
         $key[$this->getKeyName()] = $kindergarten[$this->getKeyName()];
--- app/Skole.php
+++ app/Skole.php
@@ -9,7 +9,21 @@ class Skole extends Model
     //
     public $incrementing = false;
     protected $primaryKey = 'NSRId';
-    protected $fillable = ['Kommunenr', 'Navn', 'FulltNavn', 'OrgNr', 'NSRId','ErSkole', 'ErSkoleEier', 'ErGrunnSkole', 'ErPrivatSkole','ErOffentligSkole', 'ErVideregaaendeSkole'];
+    protected $fillable = [
+        'Kommunenr',
+        'Navn',
+        'FulltNavn',
+        'OrgNr',
+        'NSRId',
+        'ErSkole',
+        'ErSkoleEier',
+        'ErGrunnSkole',
+        'ErPrivatSkole',
+        'ErOffentligSkole',
+        'ErVideregaaendeSkole',
+        'Breddegrad',
+        'Lengdegrad',
+    ];
 
     public function UpdateSkole($school) {
         $key[$this->getKeyName()] = $school[$this->getKeyName()];
