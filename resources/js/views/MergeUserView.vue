ff55db705515f2cd180ed28618ddddf12b333153
etsikt <erlend.thune@gmail.com>
2021-02-05 00:40:57 +0100

    Polish user interface.

--- resources/js/views/MergeUserView.vue
+++ resources/js/views/MergeUserView.vue
@@ -1,67 +1,94 @@
 <template>
   <div>
-    <h1>Slå sammen brukere</h1>
-    <h2>1. Generer kode</h2>
-    <p>
-    For å flytte innholdet fra denne brukeren over til en annen, f.eks. fordi du har fått ny FeideId, 
-    må du generere en sammenslåingskode ved å trykke på knappen nedenfor.
-    </p>
+    <br>
+    <div v-if="mergeError" class="alert alert-danger">{{ mergeError }}</div>
+    <div v-if="usersMerged" class='alert alert-success'>Sammenslåingen av brukerne var vellykket.</div>
 
-    <div v-if="error" class="alert alert-danger">{{ error }}</div>
-    <button
-      class="btn"
-      :class="{
-        'btn-primary': true,
-      }"
-      @click="fetchToken"
-    >
-      Lag sammenslåingskode
-    </button>
-    <span v-if="isGeneratingToken" class="ml-3">Genererer kode<div class="spinner-border text-danger"></div></span>
-
-    <div v-show="tokenGenerated" class="show-token-instructions"></div>
-    <pre v-show="tokenGenerated" class="show-token"></pre>
-
-    <h2>2. Sjekk innmeldingskonflikter</h2>
-    Dersom du har en kode fra en bruker du ønsker å slå sammen med brukeren du er logget inn med nå, skriver
-    du inn koden nedenfor.
-    <div>
-      <input type="text" id="merge-user-token" name="token" />
-    </div>
-    <div class="course-intersection">
-      Innmeldingskonflikter:
-    </div>
-    <merge-user-conflicts
-      :conflicts="conflicts"
-      :conflictsLoaded="conflictsLoaded"
-    ></merge-user-conflicts>    
-    <div>
-      <button
-        class="btn"
-        :class="{
-          'btn-primary': true,
-        }"
-        @click="fetchIntersection"
-      >
-        Finn innmeldingskonflikter
+    <div v-if="!moveContentState && !fetchContentState">
+      <h3>Hva ønsker du å gjøre?</h3>
+      <button class="kpas-button"
+            @click="moveContent"
+          >
+        Flytt innhold fra denne brukeren over til en annen
       </button>
-    <span v-if="isFetchingIntersection" class="ml-3">Sjekker konflikter<div class="spinner-border text-danger"></div></span>
-    </div>
-    <h2>3. Hent innhold</h2>
-    Når du har sjekket at det ikke er noen innmeldingskonflikter kan du slå sammen brukerne.
-    <div>
+      <br><br>
       <button
-        class="btn"
-        :disabled="!canMergeUsers"
-        :class="{
-          'btn-primary': true,
-        }"
-        @click="doMerge"
-      >
-        Slå sammen
+            class="kpas-button"
+            @click="fetchContent"
+          >
+        Hent innhold fra en annen bruker over til denne
       </button>
-    <span v-if="isMergingUsers" class="ml-3">Sjekker konflikter<div class="spinner-border text-danger"></div></span>
     </div>
+    <div v-if="moveContentState">
+      <h3>Flytt innhold til en annen bruker</h3>
+For å flytte innhold fra denne brukeren over til en annen generer du først en
+sammenslåingkode ved å trykke på knappen nedenfor.
+      <br><br>
+      <div>
+        <button
+          class="kpas-button"
+          :disabled="isGeneratingToken || tokenGenerated"
+          @click="fetchToken"
+        >
+          Generer sammenslåingskode
+        </button>
+      <span v-if="isGeneratingToken" class="ml-3">Genererer kode <div class="spinner-border text-danger"></div></span>
+      &nbsp;&nbsp;&nbsp;<span v-show="tokenGenerated" class="show-token">{{ tokenData }} </span>
+      </div>
+      <br>
+      <div v-show="tokenGenerated" class="show-token-instructions">
+        Kopier koden og start sammenslåingsverktøyet for brukeren du vil slå denne brukeren sammen med.
+        Koden er gyldig i en halvtime.
+      </div>
+    </div>
+    <div v-if="fetchContentState">
+      <h3>Hent innhold fra en annen bruker over til denne</h3>
+Skriv inn koden du har laget for brukeren du ønsker å hente innhold fra. Dersom du ikke har en slik kode
+må du først logge inn som brukeren du vil hente innhold fra og velge "Flytt innhold fra denne brukeren over til en annen"      <div>
+<br>
+        Kode: <input 
+        :disabled="canMergeUsers" 
+        type="text" 
+        size="30" 
+        id="merge-user-token" 
+        name="token" />
+<br><br>        
+      </div>
+Når du har skrevet inn koden trykker du på "Finn innmeldingskonflikter" for å undersøke om
+de to brukerne er medlem av en eller flere like kompetansepakker. I så fall må konfliktene løses.      
+        <br><br>
+        <button
+          class="kpas-button"
+          :disabled="isFetchingIntersection || canMergeUsers"
+          @click="fetchIntersection"
+        >
+          Finn innmeldingskonflikter
+        </button>
+      <span v-if="isFetchingIntersection" class="ml-3">Sjekker konflikter <div class="spinner-border text-danger"></div></span>
+      </div>
+      <br>
+      <div v-if="conflictsLoaded && !canMergeUsers">
+Følgende innmeldingskonflikter ble funnet. Du må melde deg av kompetansepakkene for en av brukerne før du kan slå de sammen.
+        <merge-user-conflicts
+          :conflicts="conflicts"
+          :conflictsLoaded="conflictsLoaded"
+        ></merge-user-conflicts>    
+      </div>
+      <div v-if="canMergeUsers">
+      Ingen innmeldingskonflikter ble funnet. Trykk på "Slå sammen" for å flytte innholdet til denne brukeren.
+      <br><br>
+        <div>
+          <button
+            class="kpas-button"
+            :disabled="isMergingUsers || usersMerged"
+            @click="doMerge"
+          >
+            Slå sammen
+          </button>
+          <span v-if="isMergingUsers" class="ml-3">Slår sammen brukerne <div class="spinner-border text-danger"></div></span>
+      </div>
+    </div>
+    <br>
   </div>
 </template>
 
@@ -80,19 +107,27 @@ export default {
       isMergingUsers: false,
       canMergeUsers: false,
       tokenGenerated: false,
+      tokenData: "",
       conflicts: null,
       conflictsLoaded: false,
-      error: "",
+      fetchContentState: false,
+      moveContentState: false,
+      usersMerged: false,
+      mergeError: "",
     };
   },
-
+  computed: {
+  	disableGenerateTokenButton: function() {
+    	return this.isGeneratingToken;
+    }
+  },
   methods: {
     clearError(errorType) {
-      this.error = "";
+      this.mergeError = "";
       this.iframeresize();
     },
     reportError(errorType, e) {     
-      this.error = e;
+      this.mergeError = e;
       this.iframeresize();
     },
     iframeresize() {
@@ -104,9 +139,17 @@ export default {
         );
       });
     },
+    moveContent() {
+      this.moveContentState = true;
+    },
+    fetchContent() {
+      this.fetchContentState = true;
+    },
     async fetchToken() {
       try {
+        this.clearError("mergeError");
         this.isGeneratingToken = true;
+        this.tokenData = "";
         console.log("Getting token");
         const token = await api.get("/user/merge/token", {
           params: { cookie: window.cookie },
@@ -114,16 +157,18 @@ export default {
         this.isGeneratingToken = false;
         this.tokenGenerated = true;
         
-        document.querySelector(".show-token-instructions").textContent = "Kopier denne koden og start sammenslåingsverktøyet for brukeren du vil slå denne brukeren sammen med:";
-        document.querySelector(".show-token").textContent = token.data;
-        this.clearError("groupError");
+        this.tokenData = token.data;
+        this.clearError("mergeError");
       } catch (e) {
         this.isGeneratingToken = false;
-        this.reportError("groupError", "Kunne ikke hente sammenslåingskode.");
+        this.reportError("mergeError", "Kunne ikke hente sammenslåingskode. " + e.response.data);
       }
     },
     async fetchIntersection() {
       try {
+        this.clearError("mergeError");
+        this.conflictsLoaded = false;
+        this.canMergeUsers = false;
         this.isFetchingIntersection = true;
         const token = document.getElementById("merge-user-token").value;
         console.log("Getting intersection with token: " + token);
@@ -138,17 +183,19 @@ export default {
         this.canMergeUsers = !intersection.data.length;
         this.conflicts = intersection.data;
         //document.querySelector(".course-intersection").textContent = "Innmeldingskonflikter: " + intersection.data;
-        this.clearError("groupError");
+        this.clearError("mergeError");
       } catch (e) {
         this.isFetchingIntersection = false;
         this.reportError(
-          "groupError",
-          "Kunne ikke hente innmeldingskonflikter."
+          "mergeError",
+          "Kunne ikke hente innmeldingskonflikter. " + e.response.data
         );
       }
     },
     async doMerge() {
       try {
+        this.clearError("mergeError");
+        this.isMergingUsers = true;
         const token = document.getElementById("merge-user-token").value;
         console.log("Merging with " + token + " into user");
 
@@ -156,11 +203,14 @@ export default {
           params: { cookie: window.cookie },
           headers: { "X-merge-token": token },
         });
+        this.isMergingUsers = false;
+        this.usersMerged = true;
 
         console.log("Merged");
-        this.clearError("groupError");
+        this.clearError("mergeError");
       } catch (e) {
-        this.reportError("groupError", e.response.data);
+        this.isMergingUsers = false;
+        this.reportError("mergeError", e.response.data);
       }
     },
   },
