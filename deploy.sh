45d0d4de6b096b13ddf6275d9ecbc44f609139dc
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2022-09-27 10:31:05 +0200

    deploy.sh composer version

--- deploy.sh
+++ deploy.sh
@@ -103,7 +103,7 @@ if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
   # shellcheck disable=SC2164
   pushd "$DEPLOYMENT_TARGET"
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
   php composer-setup.php
   php -r "unlink('composer-setup.php');"
   php composer.phar install $COMPOSER_ARGS
