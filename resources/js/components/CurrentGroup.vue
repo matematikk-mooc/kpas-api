5ad471b7934c76d7d1c9d439ba9576aa23c5dbd1
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2022-11-10 14:43:56 +0100

    async comp

--- resources/js/components/CurrentGroup.vue
+++ resources/js/components/CurrentGroup.vue
@@ -1,10 +1,10 @@
 <template>
 
-    <ul class="list-group mt-3 mb-3" v-if="groupsLoaded && groups.length > 0">
+    <ul class="list-group mt-3 mb-3" v-if="groupsLoaded && data.groups.length > 0">
       <p>your groups</p> 
         <li
           class="list-group-item"
-          v-for="(group, name) in data"
+          v-for="(group, name) in data.groups"
           v-if="group"
           :key="name"
         >
@@ -12,7 +12,7 @@
         </li>
         <div></div>
     </ul>
-    <div v-else-if="groupsLoaded && groups.length === 0"
+    <div v-else-if="groupsLoaded && data.groups.length === 0"
         class="alert alert-warning">Du er ikke med i noen grupper. <p>For å være med i gruppediskusjoner må du velge din tilhørighet lenger ned på denne siden.</p>
     </div>
     <p v-else>
@@ -33,10 +33,12 @@ import { reactive } from 'vue';
     },
     data() {
       return {
-        data : this.groups,
+        data : null,
       }
     }, 
     setup(props) {
+      this.data = props.groups;
+      console.log("in setup currentgroups " + this.data.groups.length )
       console.log(props.groups);
     }
   }
