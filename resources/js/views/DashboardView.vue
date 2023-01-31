b5e3d3eb3168cd6d73a69e8b9e86305ec26d894a
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2023-01-31 14:50:56 +0100

    KURSP-749: returning view before groups are ready
    make sure groups are ready before returning view

--- resources/js/views/DashboardView.vue
+++ resources/js/views/DashboardView.vue
@@ -75,14 +75,18 @@ export default {
         );
       });
     },
-    getBgColor() {
-      this.postMessageToParent('kpas-lti.getBgColor');
-    },
     postMessageToParent(subject) {
       const message = {
         subject: subject
       };
       window.parent.postMessage(JSON.stringify(message), "*");
+    },
+    getBgColor() {
+      this.postMessageToParent('kpas-lti.getBgColor');
+    },
+    getUsersGroups() {
+      console.log("Get users groups.");
+      this.postMessageToParent('kpas-lti.getusergroups');
     },
     connectToParent() {
       if(this.connectedToParent === true) {
@@ -91,58 +95,51 @@ export default {
       this.postMessageToParent('kpas-lti.connect');
       window.setTimeout(this.connectToParent, 500);
     },
-    getUsersGroups() {
-        console.log("Get users groups.");
-        this.postMessageToParent('kpas-lti.getusergroups');
-      },
+  
     updateModule(value){
       let index = this.modules.indexOf(value)
       this.view_module =  this.survey_data[index]
     },
     async updateCurrentGroups() {
-        console.log("updateCurrentGroups");
-        if(!this.categoriesLoaded) {
-          console.log("categories not ready yet.");
-          return;
+      console.log("updateCurrentGroups");
+      if(!this.categoriesLoaded) {
+        console.log("categories not ready yet.");
+        return;
+      }
+      if(!this.groupsLoaded) {
+        console.log("groups not ready yet.");
+        return;
+      }
+      if(this.categories && this.usersGroups) {
+        console.log("got categories and usergroups")
+        await Promise.all([this.userGroups = this.categorizeGroups(this.usersGroups, this.categories)]);
+      }
+    },
+    categorizeGroups(groups, categories) {
+      var result = {};
+      var self = this;
+      categories.forEach(function(category) {
+        var group = groups.find(group => category.id == group.group_category_id && self.course_id == group.course_id);
+        if(group) {
+          result[category.name] = group;
         }
-        if(!this.groupsLoaded) {
-          console.log("groups not ready yet.");
-          return;
-        }
-        if(this.categories && this.usersGroups) {
-          console.log("got categories and usergroups")
-          await Promise.all([this.userGroups = this.categorizeGroups(this.usersGroups, this.categories)]);
-          //this.iframeresize();
-        }
-      },
-      categorizeGroups(groups, categories) {
-        var result = {};
-        var self = this;
-        categories.forEach(function(category) {
-          var group = groups.find(group => category.id == group.group_category_id && self.course_id == group.course_id);
-          if(group) {
-            result[category.name] = group;
+      });
+      return result;
+    },
+    async getGroupCategories() {
+      try {
+        const response = await api.get('/group/user', {
+          params: {
+            cookie: window.cookie,
           }
         });
-        return result;
-      },
-       async getGroupCategories() {
-        try {
-          const response = await api.get('/group/user', {
-            params: {
-              cookie: window.cookie,
-            }
-          });
-          this.categories = response.data.result;
-          this.categoriesLoaded = true;
-          console.log("Categories received.");
-          await Promise.all([this.updateCurrentGroups()]);
-          //this.clearError("groupError");
-        } catch(e){
-          console.log("error ", e)
-          //this.reportError("groupError", "Kunne ikke hente grupper.");
-        }
-      },
+        this.categories = response.data.result;
+        this.categoriesLoaded = true;
+        console.log("Categories received.");
+      } catch(e){
+        console.log("error ", e)
+      }
+    },
     async getSurveyData() {
       try {
         let groupId = "";
@@ -177,13 +174,12 @@ export default {
   async created() {
     let self = this;
     self.course_id = self.settings.custom_canvas_course_id
-    console.log(self.settings)
-    console.log(self.likert5ops)
+    await self.getGroupCategories()
     const mql = window.matchMedia('(max-width: 500px)');
     mql.onchange = (e) => { 
       self.iframeresize();
     }
-    window.addEventListener('message', function(evt) {
+    window.addEventListener('message', async function(evt) {
       try {
         let msg = JSON.parse(evt.data);
         if(msg.subject == "kpas-lti.ltibgcolor" && msg.bgColor) {
@@ -198,20 +194,15 @@ export default {
         }else if(msg.subject == "kpas-lti.usergroups") {
           self.groupsLoaded = true;
           self.usersGroups = msg.groups;
-          self.updateCurrentGroups();
+          await self.updateCurrentGroups()
+          await self.getSurveyData()
+          self.ready = true;
         }
       } catch(e) {
         //This message is not for us.
       }
     }, false);
-    self.connectToParent();
-    await Promise.all([self.getGroupCategories()]);
-    //this.iframeresize();
-    self.getSurveyData()
-    this.ready = true
-  },
-  mounted() {
-    //this.iframeresize();
+    self.connectToParent();  
   }
 };
 </script> 
