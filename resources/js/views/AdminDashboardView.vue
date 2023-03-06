6708204f27e9f1a02d8ccd599b42b6ff2387ac2c
Lars Iver Erdal-Aase <larsen2610@gmail.com>
2023-03-06 09:52:54 +0100

    KURSP-765, KURSP-771, KURSP-772, KURSP-774: admin dashboard student count (#246)
    * KURSP-768: Admin dashboard lti
    
    Add new lti template for admin dashboard
    
    * student count wip
    
    * minor formatting
    
    * KURSP-772: survey data in admin dashboard
    
    add survey data to admin dashboard, use course module to find surveys, modules with more than one survey has a survey dropdown
    
    * filter dashboard on groupid wip
    
    * update student count by groupid
    
    * KURSP-774: add marked as done to dashboard
    
    Add marked as done per group per module item to admin dashboard
    
    * KURSP-774: Fixes after merge
    
    Fix minor issues after merging admin dashboard
    
    * KURSP-765: add middleware to groups endpoint
    
    add lti middleware
    
    * KURSP-772: update survey
    
    update survey if module is set and group changes
    
    ---------
    
    Co-authored-by: Thea Hvalen Thodesen <thea.hvatho@gmail.com>

--- resources/js/views/AdminDashboardView.vue
+++ resources/js/views/AdminDashboardView.vue
@@ -1,21 +1,256 @@
 @extends('layouts.app')
 <template>
-  <h2>Admin dashboard view</h2>
+  <div v-if="ready" >
+    <DashboardGroupSelect 
+      :settings=this.settings
+      :categories=this.categories
+      @update="updateGroupId"
+    />
+    <section id="student-count">
+      <h3>Antall brukere: {{ studentCount }}</h3>
+    </section>
+    
+  
+    <v-select
+      class="selector"
+      :disabled="!coursemodules.length"
+      label="name"
+      :options="coursemodules"
+      placeholder="Velg modul"
+      @update:modelValue="updateModule"
+    ></v-select>
+
+    <div v-if="groupId && completed_count_item.length && current_module" >
+      <h3>Markert som ferdig: </h3>
+      <section class="completed">
+        <table>
+          <tr>
+            <th><b>Side</b></th>
+            <th><b>Antall fullført</b></th>
+          </tr>
+          <tr v-for="item in completed_count_item">
+            <td>{{ item.title }}</td>
+            <td>{{ item.count }}</td>
+          </tr> 
+        </table>
+      </section>
+    </div>
+
+    <div class = "survey-data" v-if="module_surveys.length && current_module">
+
+        <v-select v-if="module_surveys.length > 1"
+        class="selector"
+        label="title_internal"
+        :options="module_surveys"
+        placeholder="Velg survey"
+        @update:modelValue="updateSurvey"
+        ></v-select>
+
+
+      <h3 class="title">{{view_survey.title_internal}}</h3>
+
+      <section class="grouped" >
+        <grouped-bar-chart id="view_module.course_id" :data="view_survey.questions.slice(0,3)" :likert5ops="this.likert5ops"></grouped-bar-chart>
+      </section>
+
+      <section class="barview" v-if="view_survey.questions.length > 4">
+        <div v-for="(question, i) in view_survey.questions.slice(3)" :key="i">
+          <bar-chart v-if="question.question_type == 'likert_scale_5pt'" :id="'q' + i" :data="question" :svgWidth=600 :svgHeight=400></bar-chart>
+        </div>
+      </section>
+
+      <section class="feedback">
+        <div v-for="(question, i) in view_survey.questions" :key="i">
+          <open-answer v-if="question.question_type === 'essay'" :openAnswers="question.submission_data" :questionText="question.text"></open-answer>
+        </div>
+      </section>
+    </div>
+    <div v-else-if="!module_surveys.length && current_module">
+      <h3> Denne modulen har ikke survey. </h3>
+    </div>
+    <div v-else>
+      <h3> Velg modul for å se survey resultater. </h3>
+    </div>
+  </div>
+  <div v-else>
+    <span class="ml-3">Laster Dashboard. <div class="spinner-border text-success"></div></span>
+  </div>
 
 </template>
 
 <script>
 import api from "../api";
+import DashboardGroupSelect from "../components/DashboardGroupSelect";
+
 export default {
   name: "AdminDashboardView",
+  components: {
+    DashboardGroupSelect,
+  },
+  data() {
+    return {
+      studentCount: null,
+      groupId: null,
+      categories: null,
+      survey_data: null,
+      module_surveys: [], 
+      view_survey: null,
+      ready: false,
+      modules_statistics: null,
+      completed_count_item: [], 
+      current_module: null,
+    }
+  },
   props: {
     settings: {},
-    likert5ops: {}
-  }
-  
+    likert5ops: {},
+    coursemodules: [],
+  },
+  methods: { 
+     async getStudentCount() {
+      try {
+        let url;
+        if (this.groupId) { 
+          url = "/group/" + this.groupId + '/count';
+        } else { 
+          url = "/course/" + this.settings.custom_canvas_course_id + '/count';
+        };
+
+        const response = await api.get(url, {
+          params: { cookie: window.cookie }
+        });
+
+        this.studentCount = await response.data.result;
+      } catch(e) {
+        console.error("Could not get student count.", e);
+      }
+    },
+    async getGroupCategories() { // TODO: refactor this method to GroupSelector
+      try {
+        const response = await api.get('/group/user', {
+          params: {
+            cookie: window.cookie,
+          }
+        });
+        this.categories = response.data.result;
+      } catch(e){
+        console.error("Could not get group categories.", e)
+      }
+    },
+    async updateGroupId(value) {
+      this.groupId = value;
+      await this.getStudentCount();
+      await this.getModulesStatistics();
+      await this.getSurveyData();
+    },
+    updateModule(value){
+      if(value == null){
+        this.current_module = null
+        return;
+      }
+      this.module_surveys = this.survey_data.filter(e => e.module_id == value.id)
+      if(this.module_surveys.length > 0){
+        this.view_survey = this.module_surveys[0];
+      }
+      this.current_module = value.id;
+      this.updateFinnishCount()
+    },
+    updateSurvey(value){
+      this.view_survey = value;
+    },
+    async getSurveyData() {
+      try {
+        let url;
+        if(!this.groupId){
+          url = "/survey/course/" + this.settings.custom_canvas_course_id;
+        }
+        else {
+          url = "/survey/course/" + this.settings.custom_canvas_course_id + "?group=" + this.groupId;
+        }
+        const apiResult = await api.get(url, {
+          params: { cookie: window.cookie }
+        });
+        this.survey_data = apiResult.data.result;
+        if(this.current_module) {
+          this.view_survey = this.survey_data.filter(e => e.module_id == this.current_module)[0]
+        }
+      } catch(e)
+      {
+        console.log("Could not get survey data.", e);
+      }
+    },
+    async getModulesStatistics(){
+      try{
+        if(this.groupId){
+          let url = "/course/" + this.settings.custom_canvas_course_id + "/modules?group=" + this.groupId;
+          const apiResult = await api.get(url, {
+            params: { cookie: window.cookie }
+          });
+          this.modules_statistics = JSON.parse(apiResult.data.result);
+          this.updateFinnishCount()
+
+        }
+      }catch(e)
+      {
+        console.log("Could not get module data.", e);
+      }
+    },
+    updateFinnishCount(){
+      this.completed_count_item = []
+      if (!this.current_module){
+        return;
+      }
+      let module = this.modules_statistics.find(e => e.canvas_id == this.current_module);
+      let moduleitems = module.module_items.sort((a, b) => a.position - b.position)
+      for(const item of moduleitems){
+        if(this.groupId) {
+          if(item.user_groups.length == 1){
+            let obj = {"title" : item.title, "count" : item.user_groups[0].count}
+            this.completed_count_item.push(obj)
+          }
+          else{
+            let obj = {"title" : item.title, "count" : 0}
+            this.completed_count_item.push(obj)
+          }
+        }
+      }      
+    }
+  },
+ 
+  async created() {
+    await this.getGroupCategories();
+    await this.getStudentCount();
+    await this.getSurveyData();
+    this.ready = true;
+  },
 };
 </script> 
 
 <style>
+ td{
+  padding: 1em;
+ }
+ 
+ h3 {
+  padding: .5em;
+ }
 
+ .selector{
+  width: 40%;
+ }
+ 
+ .survey-data{
+  width: 100%;
+ }
+
+ .completed{
+  overflow-y: scroll;
+  height: 18em;
+  margin: 1em;
+  padding: 1em;
+ }
+
+ template{
+  background-color: #eaeaea;
+ }
 </style>
--- resources/views/main/admin-dashboard.blade.php
+++ resources/views/main/admin-dashboard.blade.php
@@ -1,13 +1,18 @@
-@php use App\Models\SurveyQuestion; @endphp
+@php use App\Models\SurveyQuestion; use App\Services\CanvasService; use App\Repositories\CanvasRepository; use GuzzleHttp\Client; @endphp
 @extends('layouts.app')
 
 @section('content')
     @php
         $likertScale5ptOptions = SurveyQuestion::getLikertScaleOptions();
+        $canvasService = new CanvasService(new Client());
+        $canvasRepository = new CanvasRepository($canvasService);
+        $course_id = intval($settings["custom_canvas_course_id"]);
+        $courseModules = $canvasRepository->getCourseModules($course_id);
     @endphp
 
     <admin-dashboard-view :settings="{{ json_encode($settings) }}"
-                 :likert5ops="{{ json_encode($likertScale5ptOptions) }}">
+                 :likert5ops="{{ json_encode($likertScale5ptOptions) }}"
+                 :coursemodules="{{ json_encode($courseModules) }}" >
     </admin-dashboard-view>
 
 @endsection
