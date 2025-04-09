b998318d764314ed89b8b8ce6de3afed48b734fe
Marius <marius.johansen@udir.no>
2025-04-09 20:22:34 +0200

    DIT-449: Fonts.
    - Removed all google imports of fonts
    - Replaced all fonts with variable font from designsystem, Inter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     @vite('resources/sass/app.scss')
 
 </head>
